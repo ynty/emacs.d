c41fdccf2b473542f91518c9d94259eb1b07c4be
ynty <tn072g@outlook.com>
2025-12-28 12:37:58 +0800

    Update lisp/init-themes.el

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,17 +1,19 @@
 ;;; init-themes.el --- Defaults for themes -*- lexical-binding: t -*-
 ;;; Commentary:
 
+;; I use Modus, a built-in (since GNU Emacs 28.1) theme set designed for
+;; accessible readability.
+;;   https://protesilaos.com/emacs/modus-themes
+;; 
+;; Other themes I may try in the future:
+;;   - (straight-use-package 'catppuccin-theme)
+;;   - (straight-use-package 'solarized-theme)
+
 ;;; Code:
 
-;; Theme
-
-(load-theme 'modus-vivendi)
-
-;; (straight-use-package 'catppuccin-theme)
-;; (straight-use-package 'solarized-theme)
+(load-theme 'modus-vivendi-tinted)
 
 (provide 'init-themes)
 
 ;;; init-themes.el ends here
-
 
